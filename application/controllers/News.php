f4ccc7897d1aad969da18cd11aac21fb4f8c74f8
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2022-12-12 07:46:02 +0700

    local fixed load more

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -17,8 +17,8 @@ class News extends CI_Controller
 	{
 		$data['daerah'] = $this->data['daerah'];
 
-		test($this->news_m->getAll($this->data['daerah']['kode'],$this->input->post("limit"),$this->input->post("start")));
-		
+		// test($this->news_m->getAll($this->data['daerah']['kode'],$this->input->post("limit"),$this->input->post("start")));
+
 		$data['headline'] = $this->news_m->getHeadline();
 		$data['hukum'] = $this->news_m->getCategory("hukum dan kriminalitas");
 		$data['pendidikan'] = $this->news_m->getCategory("pendidikan");
