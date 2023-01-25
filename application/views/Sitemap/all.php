6eb65e99c7cb742269c079551b2b9b4e38e79728
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-01-25 20:36:19 +0700

    fix xml

--- application/views/Sitemap/all.php
+++ application/views/Sitemap/all.php
@@ -1,8 +1,7 @@
-<?php echo'<?xml version="1.0" encoding="UTF-8" ?>' ?>
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<?php header('Content-type: application/xml; charset="ISO-8859-1"',true); ?>
+<?php echo  '<?xml version="1.0" encoding="UTF-8"?>'?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" >
         
-        <?php foreach ($data as $key => $data) {; ?>
+  <?php foreach ($data as $key => $data) {; ?>
   <url>
    <loc><?= base_url() ?>baca/<?= $data['news_id'] ?>/<?= $this->fungsi->timeToStr("Ymd", $data['news_datepub']) ?>/<?= $this->fungsi->timeToStr("his", $data['news_datepub']) ?>/<?= $this->fungsi->convertToSlug($data['news_title']) ?></loc>
    <news:news>
@@ -13,7 +12,7 @@
    <news:publication_date><?= $this->fungsi->timeToStr("Y-m-d", $data['news_datepub']) ?></news:publication_date>
      <news:title><?= $data['news_title'] ?></news:title>
     </news:news>
-    <?php } ?>
   </url>
+    <?php } ?>
 </urlset>
 
