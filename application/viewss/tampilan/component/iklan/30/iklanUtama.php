212483c6b26a6f9d09b051cafe2e7ad9802dcf01
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-04-11 18:49:01 +0700

    hapus views dan sitemap

--- application/viewss/tampilan/component/iklan/30/iklanUtama.php
+++
@@ -1,3 +0,0 @@
-<!-- Revive Adserver Asinkron JS Menandai - Generated with Revive Adserver v5.4.1 -->
-<ins data-revive-zoneid="5" data-revive-id="117baf6d729856b93b4ce4545fb3968c"></ins>
-<script async src="//pasangiklan.jatimtimes.com/adserver/www/delivery/asyncjs.php"></script>
--- application/viewss/tampilan/component/iklan/iklanBeranda.php
+++
@@ -1,5 +0,0 @@
-<script src="https://pasangiklan.jatimtimes.com/amb/ser.php?f=33"></script>
-
-<script src="https://pasangiklan.jatimtimes.com/amb/ser.php?f=34"></script>
-
-<script src="https://pasangiklan.jatimtimes.com/amb/ser.php?f=280"></script>
