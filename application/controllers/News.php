61c29fd4b3461e1eb97be0858c3bd44bfb7d35f8
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-04-14 08:53:43 +0700

    ganti tag warna merah

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -109,7 +109,7 @@ class News extends CI_Controller
 						<div>
 							<header class="text-primary fn80 text-uppercase font-weight-bold">
 							</header>
-							<span class="badge badge-warning bg-tia50000 text-uppercase mb-1 kater" id="kater">'.$row['news_subtitle'].'</span>
+							<small class="text-uppercase text-danger mb-1" id="kater"><i class="fas fa-tags"></i>  <strong>'.$row['news_subtitle'].'</strong></small>
 							<h3>'.$row['news_title'].'</h3>
 							<div class="text-muted d-block fn60">
 								<ion-icon name="time-outline"></ion-icon>'.$this->fungsi->timeAgo($row['news_datepub']).'</span>
@@ -142,6 +142,7 @@ class News extends CI_Controller
 						<div>
 							<header class="text-primary fn80 text-uppercase font-weight-bold">'.urldecode($this->input->post("kanal")).'
 							</header>
+							<small class="text-uppercase text-danger mb-1" id="kater"><i class="fas fa-tags"></i>  <strong>'.$row['news_subtitle'].'</strong></small>
 							<h3>'.$row['news_title'].'</h3>
 							<div class="text-muted d-block fn60">
 							<ion-icon name="time-outline"></ion-icon>'.$this->fungsi->timeAgo($row['news_datepub']).'</span>
@@ -175,6 +176,7 @@ class News extends CI_Controller
 						<div>
 							<header class="text-primary fn80 text-uppercase font-weight-bold">'.urldecode($this->input->post("kanal")).'
 							</header>
+							<small class="text-uppercase text-danger mb-1" id="kater"><i class="fas fa-tags"></i>  <strong>'.$row['news_subtitle'].'</strong></small>
 							<h3>'.$row['news_title'].'</h3>
 							<div class="text-muted d-block fn60">
 							<ion-icon name="time-outline"></ion-icon>'.$this->fungsi->timeAgo($row['news_datepub']).'</span>
